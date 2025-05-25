feb4f0bc06d52dbc37b87973a0bd265e47f1bfd5
Honsage <musatov.i.a@edu.mirea.ru>
2025-05-25 23:11:29 +0300

    feat(frontend): import katex lib to display latex blocks im materials

--- frontend/src/components/blocks/latex-block/latex-block.tsx
+++ frontend/src/components/blocks/latex-block/latex-block.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import katex from 'katex';
+import 'katex/dist/katex.min.css';
 import { LatexBlock as LatexBlockType } from '../../../types/material.types';
 import * as styles from './latex-block.module.css';
 
@@ -9,11 +11,25 @@ interface LatexBlockProps {
 const LatexBlock: React.FC<LatexBlockProps> = ({ block }) => {
     const { content } = block;
 
-    // TODO: Add proper LaTeX rendering library (e.g., KaTeX or MathJax)
+    const html = katex.renderToString(content.formula, {
+        displayMode: content.displayMode,
+        throwOnError: false,
+        strict: false,
+        trust: true,
+        macros: {
+            '\\RR': '\\mathbb{R}',
+            '\\NN': '\\mathbb{N}',
+            '\\ZZ': '\\mathbb{Z}',
+            '\\CC': '\\mathbb{C}',
+            '\\QQ': '\\mathbb{Q}'
+        }
+    });
+
     return (
-        <div className={`${styles.latex} ${content.displayMode ? styles.display : styles.inline}`}>
-            {content.formula}
-        </div>
+        <div 
+            className={`${styles.latex} ${content.displayMode ? styles.display : styles.inline}`}
+            dangerouslySetInnerHTML={{ __html: html }}
+        />
     );
 };
 
